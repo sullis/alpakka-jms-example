af55b48960ac8f4df3caadb182212871526bdb53
Sean Sullivan <github@seansullivan.com>
2019-07-20 17:10:51 -0400

    implement JmsExampleSpec

--- src/test/scala/io/github/sullis/alpakka/jms/JmsExampleSpec.scala
+++ src/test/scala/io/github/sullis/alpakka/jms/JmsExampleSpec.scala
@@ -2,38 +2,89 @@ package io.github.sullis.alpakka.jms
 
 import java.util.UUID
 
+import akka.Done
 import akka.actor.ActorSystem
-import akka.stream.alpakka.jms.{JmsConsumerSettings, JmsProducerSettings}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, OverflowStrategy, Supervision}
+import akka.stream.alpakka.jms.{JmsConsumerSettings, JmsPassThrough, JmsProducerSettings, JmsTextMessage}
 import org.scalatest.{Matchers, WordSpec}
 import jmstestkit.{JmsBroker, JmsQueue}
-import akka.stream.alpakka.jms.scaladsl.{JmsConsumer, JmsProducer}
+import akka.stream.alpakka.jms.scaladsl.{JmsConsumer, JmsConsumerControl, JmsProducer}
+import akka.stream.scaladsl.{Keep, RunnableGraph, Sink, Source}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Milliseconds, Seconds, Span}
 
-class JmsExampleSpec extends WordSpec with Matchers {
+import scala.concurrent.Future
+
+class JmsExampleSpec extends WordSpec
+  with Matchers
+  with Eventually {
+
+  implicit override val patienceConfig = PatienceConfig(Span(5, Seconds), Span(15, Milliseconds))
 
   "Alpakka JMS" should {
     "happy path" in {
-
       val actorSys = ActorSystem.apply(UUID.randomUUID.toString)
-      val broker1 = JmsBroker()
-      val broker2 = JmsBroker()
+      val materializer = buildActorMaterializer(actorSys)
 
-      val queue1 = new JmsQueue(broker1)
-      val queue2 = new JmsQueue(broker2)
+      val broker = JmsBroker()
+      val sourceQueue = new JmsQueue(broker)
+      val destinationQueue = new JmsQueue(broker)
 
-      val producerSettings = JmsProducerSettings(actorSys, queue1.createConnectionFactory).withQueue(queue1.queueName)
-      val sink = JmsProducer.textSink(producerSettings)
+      sourceQueue.size shouldBe 0
+      destinationQueue.size shouldBe 0
 
-      val consumerSettings = JmsConsumerSettings(actorSys, queue1.createConnectionFactory).withQueue(queue1.queueName)
-      val source = JmsConsumer.txSource(consumerSettings)
+      val runnable = buildRunnableGraph(sourceQueue, destinationQueue, actorSys, materializer)
+      runnable.run()(materializer)
 
-      source.map { txEnvelope =>
-        System.out.println(txEnvelope.message)
-        txEnvelope.commit()
+      sourceQueue.size shouldBe 0
+      destinationQueue.size shouldBe 0
+
+      sourceQueue.publishMessage("a")
+      sourceQueue.publishMessage("b")
+      sourceQueue.publishMessage("c")
+
+      eventually {
+        destinationQueue.toSeq shouldBe Seq("a", "b", "c")
       }
 
-      broker1.stop()
-      broker2.stop()
+      sourceQueue.size shouldBe 0
+      destinationQueue.size shouldBe 3
+
+      broker.stop()
       actorSys.terminate()
     }
+
+  }
+
+  private def buildRunnableGraph(
+                     sourceQ: JmsQueue,
+                     destinationQ: JmsQueue,
+                     actorSys: ActorSystem,
+                     materializer: ActorMaterializer): RunnableGraph[(JmsConsumerControl, Future[Done])] = {
+
+    val consumerSettings = JmsConsumerSettings(actorSys, sourceQ.createConnectionFactory).withQueue(sourceQ.queueName)
+    val source = JmsConsumer.txSource(consumerSettings)
+      .map( txEnvelope => {
+        val tmsg = txEnvelope.message.asInstanceOf[javax.jms.TextMessage]
+        System.out.println("Hello: " + tmsg.getText)
+        txEnvelope.commit()
+        tmsg
+      })
+
+    val producerSettings = JmsProducerSettings(actorSys, destinationQ.createConnectionFactory).withQueue(destinationQ.queueName)
+    val sink = JmsProducer.textSink(producerSettings)
+
+    source
+      .map {
+        case t: javax.jms.TextMessage => t.getText
+        case other => sys.error("unexpected message type: " + other.getClass.getName)
+      }
+      .toMat(sink)(Keep.both)
+  }
+
+  private def buildActorMaterializer(system: ActorSystem): ActorMaterializer = {
+    val decider: Supervision.Decider = ex => Supervision.Stop
+    val matSettings = ActorMaterializerSettings(system).withSupervisionStrategy(decider)
+    ActorMaterializer(matSettings)(system)
   }
 }
